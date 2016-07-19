28a9ede25faee2823ad59821765f3e9f5c45ba0c
Thomas Nind <tznind@dundee.ac.uk>
2016-07-19 08:09:08 +0000

    Fixed namespace change caused by new CatalogueLibrary patch

--- HICPlugin/DataFlowComponents/ReadCodeAttacher.cs
+++ HICPlugin/DataFlowComponents/ReadCodeAttacher.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using CatalogueLibrary;
+using CatalogueLibrary.Data.DataLoad;
 using DataLoadEngine;
 using DataLoadEngine.Attachers;
 using DataLoadEngine.Job;
