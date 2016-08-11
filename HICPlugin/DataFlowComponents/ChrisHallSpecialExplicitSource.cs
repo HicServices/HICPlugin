ac50d9ddfec1e9dace89155d9e5ce7f9ec65367f
Thomas Nind <tznind@dundee.ac.uk>
2016-08-11 09:08:36 +0000

    Added Chris Hall Special Source

---
+++ HICPlugin/DataFlowComponents/ChrisHallSpecialExplicitSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogueLibrary.Data;
+using DataExportManager2Library.ExtractionTime.ExtractionPipeline.Sources;
+using ReusableLibraryCode.Progress;
+
+namespace HICPlugin.DataFlowComponents
+{
+    public class ChrisHallSpecialExplicitSource : ExecuteDatasetExtractionSource
+    {
+        [DemandsInitialization("The database you want a using statement put infront of")]
+        public string DatabaseToUse { get; set; }
+
+        public override string HackExtractionSQL(string sql, IDataLoadEventListener listener)
+        {
+            return "USE " + DatabaseToUse + Environment.NewLine + Environment.NewLine + base.HackExtractionSQL(sql, listener);
+
+
+        }
+    }
+}
