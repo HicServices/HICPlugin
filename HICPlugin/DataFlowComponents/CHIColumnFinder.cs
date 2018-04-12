b0ef67d37f01e6cdd729f5402459136d9cf187b9
Ross Teviotdale <rdteviotdale@dundee.ac.uk>
2018-04-12 15:54:44 +0000

    RDMPDEV-1021
    Added capability to find CHIs within text including 9 digit CHIs (missing leading zero) and currently CHIs split into 2 sections
    Added more TestCases 
    Added OverrideUntil DateTime property to choose to turn off the CHI check until a specified DateTime

--- HICPlugin/DataFlowComponents/CHIColumnFinder.cs
+++ HICPlugin/DataFlowComponents/CHIColumnFinder.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CatalogueLibrary.Data;
 using CatalogueLibrary.DataFlowPipeline;
 using HIC.Common.Validation.Constraints.Primary;
 using ReusableLibraryCode.Checks;
@@ -16,23 +17,31 @@ namespace HICPlugin.DataFlowComponents
 {
     /// <summary>
     /// Pipeline component designed to prevent DataTable columns containing CHIs passing through the pipeline. The component will crash the entire pipeline 
-    /// if it finds columns which are valid CHIs.
-    /// This will only detect values which are CHI in a whole i.e. it won't match a CHI in a list/paragraph/free text etc.
+    /// if it finds columns which contain valid CHIs.
     /// </summary>
     [Description("Crashes the pipeline if any columns are suspected of containing CHIs")]
     public class CHIColumnFinder : IPluginDataFlowComponent<DataTable>
     {
-        private int _rowCount;
+        [DemandsInitialization("Component will be shut down until this date and time", DemandType = DemandType.Unspecified)]
+        public DateTime? OverrideUntil { get; set; }
+
+        private int _rowCount = 1; //start at 1 as the first row contains the headers
 
         public DataTable ProcessPipelineData(DataTable toProcess, IDataLoadEventListener listener,
             GracefulCancellationToken cancellationToken)
         {
+            if (OverrideUntil.HasValue && OverrideUntil.Value > DateTime.Now)
+            {
+                listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, "This component is still currently being overridden until the specified date: " + OverrideUntil.Value.ToString("g")));
+                return toProcess;
+            }
+
             foreach (var row in toProcess.Rows.Cast<DataRow>())
             {
                 _rowCount++;
                 foreach (var col in toProcess.Columns.Cast<DataColumn>())
                 {
-                    if (IsValidChi(row[col]))
+                    if (ContainsValidChi(row[col]))
                         throw new Exception("Found CHI  in column " + col.ColumnName + " on row " + _rowCount + "(" + row[col] + ")");
                 }
             }
@@ -55,17 +64,35 @@ namespace HICPlugin.DataFlowComponents
             
         }
 
-        private bool IsValidChi(object toCheck)
+        private bool ContainsValidChi(object toCheck)
         {
             if (toCheck == null || toCheck == DBNull.Value)
                 return false;
 
-            var toCheckStr = toCheck.ToString().Trim();
+            var toCheckStr = toCheck.ToString();
             if (String.IsNullOrWhiteSpace(toCheckStr))
                 return false;
 
-            string outString;
-            return Chi.IsValidChi(toCheckStr, out outString);
+            var candidates = Regex.Matches(toCheckStr, @"(?<!\d)\d{10}(?!\d)|(?<!\d)\d{9}(?!\d)").Cast<Match>().Select(m => m.Value).ToList();
+
+            var regexSplitCandidates = Regex.Matches(toCheckStr, @"(?<!\d)(\d{6})(?!\d)\s(?<!\d)(\d{4})(?!\d)|(?<!\d)(\d{5})(?!\d)\s(?<!\d)(\d{4})(?!\d)").Cast<Match>().ToList();
+            var tenDigitSplit = regexSplitCandidates.Select(m => m.Groups[1].Value + m.Groups[2].Value);
+            candidates.AddRange(tenDigitSplit);
+            var nineDigitSplit = regexSplitCandidates.Select(m => m.Groups[3].Value + m.Groups[4].Value);
+            candidates.AddRange(nineDigitSplit);
+
+            foreach (var candidate in candidates.ToArray())
+            {
+                var prefix = "";
+                if (candidate.Length == 9)
+                    prefix = "0";
+
+                string outString;
+                if (Chi.IsValidChi(prefix + candidate, out outString))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
--- HICPluginTests/Unit/CHIColumnFinderTests.cs
+++ HICPluginTests/Unit/CHIColumnFinderTests.cs
@@ -13,9 +13,34 @@ namespace HICPluginTests.Unit
     public class CHIColumnFinderTests
     {
         [Test]
-        [TestCase("1111111111", true)]
-        [TestCase(" 1111111111 ", true)]
-        [TestCase("I've got a lovely bunch of 1111111111 coconuts", false)]
+        [TestCase("1111111111", true)] //valid CHI
+        [TestCase(" 1111111111 ", true)] //valid CHI with whitespace either side
+
+        [TestCase(" 09090111111111176766 ", false)] //valid CHI but as part of a longer sequence of numbers
+
+        [TestCase("I've got a lovely bunch of 1111111111 coconuts", true)] //valid CHI in amongst text
+
+        [TestCase("Not so CHI 1111111110", false)] //invalid CHI but which is 10 digits
+        [TestCase("This is my CHI:1111111113, I repeat, this is my CHI:1111111111!", true)] //an invalid CHI but which is 10 digits, then a valid 10 digit CHI with special characters and text around it
+
+        [TestCase("1111111111111", false)] //greater than 10 digits should fail (despite containing a valid chi)
+        [TestCase("This is 1111111111111 in some text", false)] //same should also fail in amongst text
+
+        [TestCase("111111110", true)] //9 digit CHI (missing leading 0)
+        [TestCase("without the initial 0! is 101010109 valid?", true)] //9 digit CHI in amongst text
+
+        [TestCase("1111111111: b j   b hfjb sbdj2009920090", true)] //valid 10 digit CHI at the start and end of the string
+        [TestCase("111111110b j   b hfjb sbdj 101010109", true)] //valid 9 digit CHI at the start and end of the string
+        [TestCase("hello1111111111   1111111111theend", true)] //valid 10 digit CHIs with text/whitespace directly before and after
+        [TestCase("hello111111110   111111110theend", true)] //valid 9 digit CHIs with text/whitespace directly before and after
+
+        [TestCase("111111 1111", true)] //valid 10 digit CHI with whitespace between dob and remaining digits 
+        [TestCase("here's some text then!111111 1111 full one 111111110", true)] //valid 10 digit CHI with whitespace between dob and remaining digits surrounded by text and another 10 digit CHI
+        [TestCase("10101 0109", true)] //valid 9 digit CHI with whitespce between dob and remaining digits
+        [TestCase("111111r1111", false)] //valid 10 digit CHI with char between dob and remaining digits
+
+        [TestCase("1111111111 101010109", true)] //valid 10 digit and valid 9 digit with whitespace between
+        [TestCase("1111111115 1111111111 101010108 111111110", true)] //invalid 10 digit, valid 10 digit, invalid 9 digit, valid 9 digit, all separated by whitespace
         public void TestDataWithCHIs(string chi, bool expectedToBeChi)
         {
             var chiFinder = new CHIColumnFinder();
