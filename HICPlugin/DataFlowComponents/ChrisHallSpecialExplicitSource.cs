57ae2f355321f6924d52056a1d661fab305747ff
Thomas Nind <tznind@dundee.ac.uk>
2018-08-27 07:25:43 +0000

    RDMPDEV-1037 added wrapper function into ChrisHallSpecialExplicitSource

--- HICPlugin/DataFlowComponents/ChrisHallSpecialExplicitSource.cs
+++ HICPlugin/DataFlowComponents/ChrisHallSpecialExplicitSource.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 using CatalogueLibrary.Data;
 using CatalogueLibrary.QueryBuilding;
+using CatalogueLibrary.Spontaneous;
+using DataExportLibrary.Data.DataTables;
 using DataExportLibrary.ExtractionTime.ExtractionPipeline.Sources;
 using ReusableLibraryCode.DatabaseHelpers.Discovery.QuerySyntax;
 using ReusableLibraryCode.Progress;
@@ -16,6 +19,15 @@ namespace HICPlugin.DataFlowComponents
         [DemandsInitialization("The collation you want injected into join SQL")]
         public string Collation { get; set; }
 
+        [DemandsInitialization(@"Sql to wrap columns that have an ANOTable_ID transform configured on them (or match AlsoANORegex) , use
+{0} - ColumnSql e.g. UPPER([mytable]..[gp_code])
+{1} - UnderlyingColumnRuntimeName e.g. gp_code
+{2} - ProjectNumber e.g. 23")]
+        public string ANOWrapFunction { get; set; }
+
+        [DemandsInitialization(@"If you have an ANOWrapFunction function then columns matching this regex will also get wrapped")]
+        public StandardRegex AlsoANORegex { get; set; }
+
         public override string HackExtractionSQL(string sql, IDataLoadEventListener listener)
         { 
             StringBuilder sb = new StringBuilder();
@@ -26,11 +38,58 @@ namespace HICPlugin.DataFlowComponents
             if (!string.IsNullOrWhiteSpace(DatabaseToUse))
                 sb.AppendLine("USE " + DatabaseToUse);
 
+            if (!string.IsNullOrWhiteSpace(ANOWrapFunction))
+                ApplyANOWrap(listener);
+
             sb.AppendLine(Request.QueryBuilder.SQL);
 
             listener.OnNotify(this,new NotifyEventArgs(ProgressEventType.Information, "HACKED SQL" + Environment.NewLine + "------------------------------------------" + Environment.NewLine + sb));
 
             return sb.ToString();
+        }
+
+        private void ApplyANOWrap(IDataLoadEventListener listener)
+        {
+            Regex regex = null;
+            if(AlsoANORegex != null)
+                regex = new Regex(AlsoANORegex.Regex,RegexOptions.IgnoreCase);
+
+            foreach (var queryTimeColumn in Request.QueryBuilder.SelectColumns)
+            {
+                if (queryTimeColumn.UnderlyingColumn != null && queryTimeColumn.UnderlyingColumn.ANOTable_ID != null)
+                    ApplyANOWrap(queryTimeColumn, listener);
+                else 
+                    if(regex != null && regex.IsMatch(queryTimeColumn.IColumn.GetRuntimeName()))
+                        ApplyANOWrap(queryTimeColumn, listener);
+                    else
+                    {
+                        listener.OnNotify(this,new NotifyEventArgs(ProgressEventType.Information, "No Match:" + queryTimeColumn));   
+                    }
+            }
+            
+            Request.QueryBuilder.RegenerateSQL();
+        }
+
+        private void ApplyANOWrap(QueryTimeColumn queryTimeColumn, IDataLoadEventListener listener)
+        {
+            var ec = queryTimeColumn.IColumn as ExtractableColumn;
+                
+            if(ec == null)
+            {
+                listener.OnNotify(this,new NotifyEventArgs(ProgressEventType.Error, "Column " + queryTimeColumn.IColumn + " matched ANO pattern or had ANO transform but wasn't an ExtractionInformation (it was a " + queryTimeColumn.IColumn.GetType()+")"));
+                return;
+            }
+
+            listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, "Match, Wrapping:" + queryTimeColumn));
+
+            var runtimeName = queryTimeColumn.IColumn.GetRuntimeName();
+
+            ec.SelectSQL = string.Format(ANOWrapFunction,queryTimeColumn.IColumn.SelectSQL,queryTimeColumn.UnderlyingColumn.GetRuntimeName(),Request.Salt);
+
+            if(string.IsNullOrWhiteSpace(queryTimeColumn.IColumn.Alias))
+                ec.Alias = runtimeName;
+
+            //IMPORTANT: Do not save this object!
         }
     }
 }
