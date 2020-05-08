c686888aca3e536d5cb8beb61e2515b886743fb6
Leandro Tramma <ltramma@dundee.ac.uk>
2020-05-08 14:06:41 +0000

    RDMP Plugin update

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
-[assembly: AssemblyInformationalVersion("2.0.1")]
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: AssemblyInformationalVersion("2.0.2")]
