6f72740ac7cf3989480c91ad60066c9806a6204e
Thomas Nind <TZNind@dundee.ac.uk>
2022-05-31 12:35:09 +0100

    5.0.5

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("5.0.4")]
-[assembly: AssemblyFileVersion("5.0.4")]
-[assembly: AssemblyInformationalVersion("5.0.4")]
+[assembly: AssemblyVersion("5.0.5")]
+[assembly: AssemblyFileVersion("5.0.5")]
+[assembly: AssemblyInformationalVersion("5.0.5")]
