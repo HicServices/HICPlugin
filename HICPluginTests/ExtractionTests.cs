79e448547aa49b0b037c8904574a0c5992719b1e
James Friel <jfriel001@dundee.ac.uk>
2023-10-04 10:52:21 +0100

    fix tests

--- HICPluginTests/ExtractionTests.cs
+++ HICPluginTests/ExtractionTests.cs
@@ -81,6 +81,7 @@ public class ExtractionTests : DatabaseTests
                 DatasetName = new Regex(".*"),
                 FilenameColumnName = "Image_Filename",
                 ImageUriColumnName = "ImageArchiveUri",
+                FileNameReplacementColumns="Examination_Date,Image_Num",
                 PathToImageArchive = archiveDir.FullName
             };
 
@@ -157,7 +158,9 @@ public class ExtractionTests : DatabaseTests
                 DatasetName = new Regex(".*"),
                 FilenameColumnName = "Image_Filename",
                 ImageUriColumnName = "ImageArchiveUri",
-                PathToImageArchive = archiveDir.FullName
+                FileNameReplacementColumns="Examination_Date,Image_Num",
+                PathToImageArchive = archiveDir.FullName,
+                AppendIndexCountToFileName=false
             };
 
             extractionComponent.PreInitialize(request, listener);
