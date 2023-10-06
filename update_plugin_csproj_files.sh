6947e633dc9a0fa88222a45bf11c51d6b3a2fd07
James Friel <jfriel001@dundee.ac.uk>
2023-10-06 09:34:09 +0100

    fix script

--- update_plugin_csproj_files.sh
+++ update_plugin_csproj_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ID=$(cat settings.yml | yq .id)
-sed -e "s|<Product>.*</Product>|<Product>$ID</Product>|g" ./Plugin/windows/windows.csproj > ./Plugin/windows/windows.csproj
-sed -e "s|<AssemblyTitle>.*</AssemblyTitle>|<AssemblyTitle>$ID</AssemblyTitle>|g" ./Plugin/windows/windows.csproj > ./Plugin/windows/windows.csproj
-sed -e "s|<Product>.*</Product>|<Product>$ID</Product>|g" ./Plugin/main/main.csproj > ./Plugin/main/main.csproj
-sed -e "s|<AssemblyTitle>.*</AssemblyTitle>|<AssemblyTitle>$ID</AssemblyTitle>|g" ./Plugin/main/main.csproj > ./Plugin/main/main.csproj
+sed -e "s|<Product>.*</Product>|<Product>$ID</Product>|g" ./Plugin/windows/windows.csproj > _temp.txt
+cat _temp.txt > ./Plugin/windows/windows.csproj
+sed -e "s|<AssemblyTitle>.*</AssemblyTitle>|<AssemblyTitle>$ID</AssemblyTitle>|g" ./Plugin/windows/windows.csproj > _temp
+cat _temp.txt > ./Plugin/windows/windows.csproj
+sed -e "s|<Product>.*</Product>|<Product>$ID</Product>|g" ./Plugin/main/main.csproj > _temp.txt
+cat _temp.txt > ./Plugin/main/main.csproj
+sed -e "s|<AssemblyTitle>.*</AssemblyTitle>|<AssemblyTitle>$ID</AssemblyTitle>|g" ./Plugin/main/main.csproj > _temp.txt
+cat _temp.txt > ./Plugin/main/main.csproj
+rm _temp.txt
