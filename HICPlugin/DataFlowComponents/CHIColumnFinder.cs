d7937207f88512fe74a47887b84b77152de67ba8
James Friel <jfriel001@dundee.ac.uk>
2023-10-26 11:46:26 +0100

    add write to file

--- HICPlugin/DataFlowComponents/CHIColumnFinder.cs
+++ HICPlugin/DataFlowComponents/CHIColumnFinder.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using Rdmp.Core.CommandExecution;
 using Rdmp.Core.Curation.Data;
 using Rdmp.Core.DataExport.DataExtraction.Commands;
@@ -12,12 +14,12 @@ using Rdmp.Core.ReusableLibraryCode;
 using Rdmp.Core.ReusableLibraryCode.Annotations;
 using Rdmp.Core.ReusableLibraryCode.Checks;
 using Rdmp.Core.ReusableLibraryCode.Progress;
+using SixLabors.ImageSharp.Drawing;
 
 namespace HICPluginInteractive.DataFlowComponents;
 
 /// <summary>
-/// Pipeline component designed to prevent DataTable columns containing CHIs passing through the pipeline. The component will crash the entire pipeline
-/// if it finds columns which contain valid CHIs.
+/// Pipeline component designed to prevent DataTable columns containing CHIs passing through the pipeline.
 /// </summary>
 [Description("Crashes the pipeline if any columns are suspected of containing CHIs")]
 public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable>, IPipelineRequirement<IExtractCommand>, IPipelineRequirement<IBasicActivateItems>
@@ -35,6 +37,9 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
         get => string.Join(',', _columnGreenList);
         set => _columnGreenList = (value ?? "").Split(',').Select(static s => s.Trim()).ToList();
     }
+    [DemandsInitialization("If populated, RDMP will output and potential CHIs to a file in this directory rather than to the progress logs", DemandType = DemandType.Unspecified)]
+
+    public string OutputFileDirectory { get; set; }
 
     private bool _firstTime = true;
 
@@ -44,6 +49,8 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
 
     public DataTable ProcessPipelineData(DataTable toProcess, IDataLoadEventListener listener, GracefulCancellationToken cancellationToken)
     {
+
+        string fileLocation = string.IsNullOrWhiteSpace(OutputFileDirectory) ? null : System.IO.Path.Combine(OutputFileDirectory, toProcess.TableName.ToString() + ".csv").ToString();
         if (OverrideUntil.HasValue && OverrideUntil.Value > DateTime.Now)
         {
             if (_firstTime)
@@ -61,11 +68,16 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
             listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
                 $"You have chosen the following columns to be ignored: {IgnoreColumns}"));
 
+        var ChiLocations = new List<string>();
         foreach (var col in toProcess.Columns.Cast<DataColumn>().Where(c => !_columnGreenList.Contains(c.ColumnName.Trim())))
         {
             foreach (var val in toProcess.Rows.Cast<DataRow>().Select(DeRef).AsParallel().Where(ContainsValidChi))
             {
-                if (_activator?.IsInteractive == true && ShowUIComponents)
+                if (!string.IsNullOrWhiteSpace(fileLocation))
+                {
+                    ChiLocations.Add($"{col.ColumnName},{GetPotentialCHI(val)},{val}");
+                }
+                else if (_activator?.IsInteractive == true && ShowUIComponents)
                 {
                     if (DoTheMessageBoxDance(toProcess, listener, col, val))
                         break; // End processing of this whole column
@@ -78,6 +90,32 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
                     if (!_isTableAlreadyNamed)
                         listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Warning,
                             "DataTable has not been named. If you want to know the dataset that the error refers to please add an ExtractCatalogueMetadata to the extraction pipeline."));
+                }
+            }
+            if (ChiLocations.Count != 0)
+            {
+                ReaderWriterLock locker = new ReaderWriterLock();
+                try
+                {
+                    locker.AcquireWriterLock(int.MaxValue);
+                    if (!File.Exists(fileLocation))
+                    {
+                        using (StreamWriter sw = File.CreateText(fileLocation))
+                        {
+                            sw.WriteLine("Column,Potential CHI,Value");
+                        }
+                    }
+                    File.AppendAllLines(fileLocation, ChiLocations);
+                }
+                catch (Exception e)
+                {
+                    listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Warning, e.Message));
+                }
+                finally
+                {
+                    locker.ReleaseWriterLock();
+                    listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, $"Have Written {ChiLocations.Count} Potential CHIs to {fileLocation}"));
+
                 }
             }
 
@@ -164,6 +202,112 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
         MaybeSpace,
         Complete
     }
+
+    private static string GetPotentialCHI(string toCheckStr)
+    {
+        if (string.IsNullOrWhiteSpace(toCheckStr) || toCheckStr.Length < 9) return "";
+
+        var state = State.End; // Start of potential CHI
+        int day = 0, month = 0, year = 0, check = 0,indexOfDay=0;
+        for (var i = toCheckStr.Length - 1; i >= 0; i--)
+        {
+            var c = toCheckStr[i];
+            var digit = c - '0';
+            if (digit is < 0 or > 9)
+            {
+                // Non-whitespace: if we're anywhere other than maybe-space, bail.
+                switch (state)
+                {
+                    case State.MaybeSpace:
+                        state = char.IsWhiteSpace(c) ? State.Year2 : State.End;
+                        break;
+
+                    case State.Day1: // Might be a 9 digit "CHI" with leading zero removed
+                    case State.Complete:
+                        state = State.End;
+                        if (ValidBits(day, month, year, check))
+                            return toCheckStr.Substring(i+1, 9);
+
+                        break;
+
+                    default:
+                        state = State.End;
+                        break;
+                }
+                continue;
+            }
+
+            // OK, we got a digit. What does it mean in the current state?
+            switch (state)
+            {
+                case State.End:
+                    check = digit;
+                    state = State.Rest3;
+                    break;
+
+                case State.Rest3:
+                    check += digit * 2;
+                    state = State.Rest2;
+                    break;
+
+                case State.Rest2:
+                    check += digit * 3;
+                    state = State.Rest1;
+                    break;
+
+                case State.Rest1:
+                    check += digit * 4;
+                    state = State.MaybeSpace;
+                    break;
+
+                case State.MaybeSpace:
+                case State.Year2:
+                    check += digit * 5;
+                    year = digit;
+                    state = State.Year1;
+                    break;
+
+                case State.Year1:
+                    check += digit * 6;
+                    year += digit * 10;
+                    state = State.Month2;
+                    break;
+
+                case State.Month2:
+                    check += digit * 7;
+                    month = digit;
+                    state = State.Month1;
+                    break;
+
+                case State.Month1:
+                    check += digit * 8;
+                    month += digit * 10;
+                    state = State.Day2;
+                    break;
+
+                case State.Day2:
+                    check += digit * 9;
+                    day = digit;
+                    indexOfDay = i;
+                    state = State.Day1;
+                    break;
+
+                case State.Day1:
+                    check += digit * 10;
+                    day += 10 * digit;
+                    indexOfDay = i;
+                    state = State.Complete;
+                    break;
+
+                case State.Complete:
+                    // More than 10 digits - just keep consuming, cannot possibly be valid now.
+                    day = 32;
+                    break;
+            }
+        }
+        return ValidBits(day, month, year, check) ? toCheckStr.Substring(indexOfDay, 10) : "";
+    }
+
     private static bool ContainsValidChi([CanBeNull] object toCheck)
     {
         if (toCheck == null || toCheck == DBNull.Value)
