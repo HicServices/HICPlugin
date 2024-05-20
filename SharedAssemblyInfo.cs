036e656422cc7fbbca30c2943902f3e7ab07114c
James Friel <JFriel001@dundee.ac.uk>
2024-05-20 14:21:20 +0100

    bump version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -10,6 +10,6 @@
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("6.1.4")]
-[assembly: AssemblyFileVersion("6.1.4")]
-[assembly: AssemblyInformationalVersion("6.1.4")]
+[assembly: AssemblyVersion("6.1.5")]
+[assembly: AssemblyFileVersion("6.1.5")]
+[assembly: AssemblyInformationalVersion("6.1.5")]
