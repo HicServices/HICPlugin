e36c6b42b4082f564eed1902af4590acaa9b1fba
James Friel <JFriel001@dundee.ac.uk>
2026-03-20 10:15:08 +0000

    tidy up

--- HICPlugin/DataFlowComponents/CHIColumnFinder.cs
+++ HICPlugin/DataFlowComponents/CHIColumnFinder.cs
@@ -62,15 +62,11 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
         }
 
 
-        listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
-                   $"Keys: {string.Join(',',_allowLists.Keys)}"));
         List<string> columnGreenList = new();
         if (_allowLists.TryGetValue(RdmpAll, out var _extractionSpecificAllowances))
             columnGreenList.AddRange(_extractionSpecificAllowances);
         if (_allowLists.TryGetValue(toProcess.TableName, out var _catalogueSpecificAllowances))
             columnGreenList.AddRange(_catalogueSpecificAllowances.ToList());
-        listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
-                   $"Found CGL: {string.Join(',',columnGreenList)} for {toProcess.TableName}"));
         var count = 0;
         string fileLocation = null;
         if (_outputFileDirectory?.Exists == true)
@@ -438,18 +434,14 @@ public sealed partial class CHIColumnFinder : IPluginDataFlowComponent<DataTable
                 listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information,
                     $"Ignoring the following columns as they have been hashed on release: {string.Join(", ", hashOnReleaseColumns)}"));
             }
-            listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, $"HIT PREINIT. {AllowListFile}, {_allowLists.Count}"));
 
             if (File.Exists(AllowListFile) && _allowLists.Count == 0)
             {
                 var allowListFileContent = File.ReadAllText(AllowListFile);
                 var deserializer = new DeserializerBuilder().Build();
                 var yamlObject = deserializer.Deserialize<Dictionary<string, List<string>>>(allowListFileContent);
-                listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, allowListFileContent));
-
                 foreach (var (catalogue, columns) in yamlObject)
                 {
-                    listener.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, $"Adding {catalogue}: {string.Join(',',columns)}"));
                     _allowLists.Add(catalogue, columns);
                 }
             }
